082125808bf4a9f48762dd5bf2c6d2d87bf448c6
Takayama Fumihiko <tekezo@pqrs.org>
2018-12-22 02:22:07 +0900

    version 2.0.0

--- include/pqrs/cf/run_loop_thread.hpp
+++ include/pqrs/cf/run_loop_thread.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::cf::run_loop_thread v1.5
+// pqrs::cf::run_loop_thread v2.0
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
